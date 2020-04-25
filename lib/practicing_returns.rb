ac193798f1db17fd6166ab868995aaf4d9243d6e
Jared  E <jared.epps322@gmail.com>
2020-04-25 00:57:31 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,14 +1,3 @@
 require "pry"
 
-def hello(array)
-  i = 0 
-  collection = []
-  while i < array.length
-  collection
-  
-  yield(array[i])
-  i += 1
-  
-end 
-end 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
